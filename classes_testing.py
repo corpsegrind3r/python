bb1f502b4928ca277d81eba8ee7b58578399d1eb
corpsegrind3r <42455570+corpsegrind3r@users.noreply.github.com>
2020-05-07 18:12:50 -0600

    Uploaded files for first use of Github :)
    Am I doing this right?

--- classes_testing.py
+++ classes_testing.py
@@ -1,3 +1,36 @@
 # working with classes
 
-class 
+#Class Snowboard
+class Snowboard:
+
+    #Class variables
+    length = 0
+    make = ''
+    model = ''
+    width = 0
+
+    #Init method or constructor
+    def __init__(self, length, make, model, width,):
+        #Instance Variables
+        self.length = length
+        self.make = make
+        self.model = model
+        self.width = width
+
+    def print_details(self):
+        print(f"This is a {self.length} {self.make} {self.model}")
+
+    def ollie(self):
+        print("Niiice ollie!")
+
+    def spin(self):
+        print("Sweet spins!")
+
+#Instantiate a snowboard
+MySnowboard = Snowboard(152,'Never Summer','Proto Type Two',30)
+
+#Let's do some methods!
+MySnowboard.print_details()
+MySnowboard.ollie()
+MySnowboard.spin()
+MySnowboard.spin()
--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,3 @@
 print("Hello Python world!")
+
+print("Let's test some Github commits and merges!")
