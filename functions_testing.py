7dd2d2ceb8f0e494bd85a7c16564d29a600b9654
corpsegrind3r <yankeessuck_18@hotmail.com>
2020-05-09 16:39:30 -0600

    Let's goooo! Working on HTML/CSS today.

--- functions_testing.py
+++ functions_testing.py
@@ -1,3 +1,4 @@
+#-------------------------------------------------------------------
 # def build_snowboard (snowboard_make, snowboard_model):
 # 	"""Building a snowboard"""
 # 	print(f"I own a {snowboard_make} {snowboard_model}")
@@ -6,16 +7,19 @@
 # build_snowboard('Never Summer','Proto Type Two')
 
 
-a = 1
-b = -1
-c = 3
 
-def equality_test(a, b):
-	if a < b:
-		print(f"{a} is less than {b}")
-	elif a == b:
-		print(f"{a} is equal to {b}")
-	else:
-		print(f"{a} is greater than {b}")
+# --------------------------------------------------------------------
+# a = 1
+# b = -1
+# c = 3
+#
+# def equality_test(a, b):
+# 	if a < b:
+# 		print(f"{a} is less than {b}")
+# 	elif a == b:
+# 		print(f"{a} is equal to {b}")
+# 	else:
+# 		print(f"{a} is greater than {b}")
+#
+# equality_test(a,b)
 
-equality_test(a,b)
--- input_testing.py
+++ input_testing.py
@@ -1,4 +1,6 @@
 # playing with input()
 
 i = input("Please enter some shit! ")
-print(f"That's all you gotta say for yourself!? Is '{i}' ?? ... that's some lame shit!")
+print(f"That's all you gotta say for yourself!? Is '{i}' ??" + \
+        "... that's some lame shit!")
+
